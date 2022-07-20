c8b05fcdd94b539163f9021f49b702331bf3c2bd
EvgenJurgen <yan4enko.zhenia@yandex.by>
2022-07-20 14:47:04 +0300

    [develop]: improved Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,108 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Search, Icon, Profile, UserIcon } from './styles';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+import {
+  Container,
+  Search,
+  SearchIcon,
+  Profile,
+  UserIcon,
+  SearchButton,
+  GoBackIcon,
+  MobileSearchContainer,
+  DropDown,
+  DropDownItem,
+} from './styles';
+
+const BIG_SIZE_LOGO_URL = 'https://www.freepnglogos.com/uploads/netflix-logo-0.png';
+const SMALL_SIZE_LOGO_URL = 'https://www.freepnglogos.com/uploads/netflix-logo-circle-png-5.png';
 
 const SEARCH_PLACEHOLDER = "I'm searching for...";
 
-export const Header = () => (
-  <Container>
-    <Link to="/">
-      <img
-        src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
-        alt="Netflix"
-        height="35"
-        width="112"
-      />
-    </Link>
-    <Search>
-      <Icon />
-      <input type="text" placeholder={SEARCH_PLACEHOLDER} />
-    </Search>
+const DROPDOWN_LINKS = [{ title: 'Logout', link: '' }];
 
-    <Profile>
-      <UserIcon />
-    </Profile>
-  </Container>
-);
+export const Header = () => {
+  const [title, setTitle] = useState('');
+  const [showInput, setShowInput] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const { width } = useWindowDimensions();
+
+  const LogoImg = useMemo(() => {
+    if (width > 576) {
+      return <img src={BIG_SIZE_LOGO_URL} alt="Netflix" height="35" width="112" />;
+    }
+    return <img src={SMALL_SIZE_LOGO_URL} alt="Netflix" height="45" width="60" />;
+  }, [width]);
+
+  const handleChangeTitle = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setTitle(event.currentTarget.value);
+  };
+
+  const handleOpenCloseInputTitle = () => {
+    setShowInput((prewState) => !prewState);
+  };
+
+  useEffect(() => {
+    if (width > 576 && showInput === true) {
+      setShowInput(false);
+    }
+  }, [width, showInput]);
+
+  const handleOpenCloseDropdown = () => {
+    setShowDropdown((prewState) => !prewState);
+  };
+
+  return (
+    <>
+      {showInput && (
+        <MobileSearchContainer>
+          <GoBackIcon onClick={handleOpenCloseInputTitle} />
+          <Search>
+            <SearchIcon />
+            <input
+              type="text"
+              placeholder={SEARCH_PLACEHOLDER}
+              value={title}
+              onChange={handleChangeTitle}
+            />
+          </Search>
+        </MobileSearchContainer>
+      )}
+      {!showInput && (
+        <Container>
+          <Link to="/">{LogoImg}</Link>
+
+          {width > 576 ? (
+            <Search>
+              <SearchIcon />
+              <input
+                type="text"
+                placeholder={SEARCH_PLACEHOLDER}
+                value={title}
+                onChange={handleChangeTitle}
+              />
+            </Search>
+          ) : (
+            <SearchButton>
+              <SearchIcon onClick={handleOpenCloseInputTitle} />
+            </SearchButton>
+          )}
+
+          <Profile>
+            <UserIcon onClick={handleOpenCloseDropdown} />
+            {showDropdown && (
+              <DropDown>
+                {DROPDOWN_LINKS.map((dropdownLink) => (
+                  <DropDownItem key={dropdownLink.title} onClick={handleOpenCloseDropdown}>
+                    {dropdownLink.title}
+                  </DropDownItem>
+                ))}
+              </DropDown>
+            )}
+          </Profile>
+        </Container>
+      )}
+    </>
+  );
+};
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,28 +1,35 @@
 import styled from 'styled-components';
 import { TbSearch } from 'react-icons/tb';
 import { FaRegUser } from 'react-icons/fa';
+import { BiArrowBack } from 'react-icons/bi';
 
 export const Container = styled.div`
   display: flex;
   align-items: center;
   padding: 1rem 3rem;
+
+  @media (max-width: 576px) {
+    padding: 0.5rem 1rem;
+  }
+`;
+
+export const MobileSearchContainer = styled(Container)`
+  width: 100%;
+  padding: 0.5rem 0.5rem;
 `;
 
 export const Search = styled.div`
-  /* width: 17rem; */
   display: flex;
-  /* margin-left: 10rem; */
-  /* border-radius: 40px; */
   border: 1px solid ${(props) => props.theme.colors.thirdTextColor};
   background-color: ${(props) => props.theme.colors.secondBackgroundColor};
-  opacity: 0.5;
-  /* padding: 0.5rem 1rem; */
+  /* opacity: 0.5; */
 
   input {
+    width: 100%;
     background-color: transparent;
     outline: none;
     color: ${(props) => props.theme.colors.thirdTextColor};
-    /* margin-left: 0.5rem; */
+    margin-left: 0.5rem;
     border: none;
     &::placeholder {
       color: ${(props) => props.theme.colors.thirdTextColor};
@@ -30,27 +37,17 @@ export const Search = styled.div`
   }
 
   @media (max-width: 576px) {
-    width: 0;
-    border: none;
-    margin-left: 1rem;
+    width: 70%;
+    margin-left: 0.5rem;
     border-radius: 40px;
     padding: 0.5rem 1rem;
-
-    input {
-      margin-left: 0.5rem;
-      width: 0;
-    }
   }
 
-  @media (min-width: 576px) and (max-width: 768px) {
+  @media (min-width: 577px) and (max-width: 768px) {
     width: 12rem;
     margin-left: 4rem;
     border-radius: 40px;
     padding: 0.5rem 1rem;
-
-    input {
-      margin-left: 0.5rem;
-    }
   }
 
   @media (min-width: 768px) and (max-width: 992px) {
@@ -58,37 +55,49 @@ export const Search = styled.div`
     margin-left: 5rem;
     border-radius: 40px;
     padding: 0.5rem 1rem;
-
-    input {
-      margin-left: 0.5rem;
-    }
   }
 
-  @media (min-width: 992px) and (max-width: 1200px) {
+  @media (min-width: 992px) {
     width: 17rem;
     margin-left: 10rem;
     border-radius: 40px;
     padding: 0.5rem 1rem;
-
-    input {
-      margin-left: 0.5rem;
-    }
-  }
-
-  @media (min-width: 1200) {
-    width: 17rem;
-    margin-left: 10rem;
-    border-radius: 40px;
-    padding: 0.5rem 1rem;
-
-    input {
-      margin-left: 0.5rem;
-    }
   }
 `;
 
-export const Icon = styled(TbSearch)`
+export const SearchButton = styled.div`
+  display: flex;
+  border: 1px solid ${(props) => props.theme.colors.thirdTextColor};
+  background-color: ${(props) => props.theme.colors.secondBackgroundColor};
+  /* opacity: 0.5; */
+
+  width: 2.5rem;
+  height: 2.5rem;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+
+  svg {
+    margin: 0 auto;
+  }
+`;
+
+export const SearchIcon = styled(TbSearch)`
   color: ${(props) => props.theme.colors.thirdTextColor};
+`;
+
+export const UserIcon = styled(FaRegUser)`
+  width: 65%;
+  height: 65%;
+  color: ${(props) => props.theme.colors.secondBackgroundColor};
+`;
+
+export const GoBackIcon = styled(BiArrowBack)`
+  color: ${(props) => props.theme.colors.thirdTextColor};
+  /* opacity: 0.5; */
 `;
 
 export const Profile = styled.div`
@@ -98,14 +107,28 @@ export const Profile = styled.div`
   width: 2.5rem;
   border-radius: 50%;
   background-color: ${(props) => props.theme.colors.thirdTextColor};
-  opacity: 0.4;
+  /* opacity: 0.4; */
   display: flex;
   align-items: center;
   justify-content: center;
+  cursor: pointer;
+
+  @media (max-width: 576px) {
+    right: 1rem;
+  }
 `;
 
-export const UserIcon = styled(FaRegUser)`
-  width: 65%;
-  height: 65%;
-  color: ${(props) => props.theme.colors.secondBackgroundColor};
+export const DropDown = styled.div`
+  position: absolute;
+  top: 3rem;
+  color: ${(props) => props.theme.colors.thirdTextColor};
+`;
+
+export const DropDownItem = styled.div`
+  margin-top: 0.5rem;
+  text-align: center;
+
+  &:hover {
+    opacity: 0.7;
+  }
 `;
