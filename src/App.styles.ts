ae091534ca8ff53aab7908c79730e004b5c7b779
vyacheslavMatyuk <v.matyuk@8allocate.com>
2022-08-01 15:17:29 +0300

    [develop]: fix warning of import font within createGlobalStyle

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -6,7 +6,6 @@ const inputStyles = css`
 `;
 
 export const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
 * {
     box-sizing: border-box;
     margin: 0px;
