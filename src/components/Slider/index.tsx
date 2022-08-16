f3dfe20ecec32d7b0f9c6d3b0667c43bd6b1ce01
EvgenJurgen <yan4enko.zhenia@yandex.by>
2022-08-16 09:59:02 +0300

    [develop]: fixed slide scrolling

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useRef } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import { AppButton } from '@components/AppButton';
 import { AppText } from '@components/AppText';
 import * as Styles from './styles';
@@ -8,15 +8,25 @@ export const Slider: FC<Props> = ({ slides, onClick }) => {
   const sliderRef = useRef<HTMLDivElement>(null);
   const sliderCardRef = useRef<HTMLDivElement>(null);
 
+  const [numberOfscrollableSlides, setNumberOfscrollableSlides] = useState(0);
+
+  useEffect(() => {
+    if (sliderRef && sliderRef.current && sliderCardRef && sliderCardRef.current) {
+      setNumberOfscrollableSlides(
+        Math.trunc(sliderRef.current.clientWidth / sliderCardRef.current.clientWidth),
+      );
+    }
+  }, [sliderRef, sliderCardRef]);
+
   const handleSlideRight = () => {
     if (sliderRef && sliderRef.current && sliderCardRef && sliderCardRef.current) {
-      sliderRef.current.scrollLeft += sliderCardRef.current.offsetWidth;
+      sliderRef.current.scrollLeft += sliderCardRef.current.offsetWidth * numberOfscrollableSlides;
     }
   };
 
   const handleSlideLeft = () => {
     if (sliderRef && sliderRef.current && sliderCardRef && sliderCardRef.current) {
-      sliderRef.current.scrollLeft -= sliderCardRef.current.offsetWidth;
+      sliderRef.current.scrollLeft -= sliderCardRef.current.offsetWidth * numberOfscrollableSlides;
     }
   };
 
@@ -27,10 +37,10 @@ export const Slider: FC<Props> = ({ slides, onClick }) => {
   return (
     <Styles.Container>
       <Styles.SliderIconLeft onClick={handleSlideLeft} />
-      <Styles.SliderContainer ref={sliderCardRef}>
+      <Styles.SliderContainer>
         <Styles.Slider ref={sliderRef}>
           {slides.map((slide) => (
-            <Styles.SliderCard key={slide.id}>
+            <Styles.SliderCard key={slide.id} ref={sliderCardRef}>
               <Styles.SliderCardImage src={slide.image} alt="logo" />
               <Styles.SliderCardInfo>
                 <AppText text={slide.name} tag="h1" />
